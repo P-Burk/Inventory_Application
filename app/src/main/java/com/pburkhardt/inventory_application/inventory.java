0056555f70f9ca069f705c8505248b0c453dd4bc
P-Burk <68164093+P-Burk@users.noreply.github.com>
2022-10-02 15:40:45 -0400

    added toolbar/appbar things

--- app/src/main/java/com/pburkhardt/inventory_application/inventory.java
+++ app/src/main/java/com/pburkhardt/inventory_application/inventory.java
@@ -1,6 +1,7 @@
 package com.pburkhardt.inventory_application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,6 +11,8 @@ public class inventory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
-
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.inventoryToolBar);
+        setSupportActionBar(myToolbar);
+        //setTitle("Inventory");
     }
 }
