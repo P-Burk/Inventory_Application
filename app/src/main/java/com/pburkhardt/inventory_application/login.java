99badcd9721bda2c5ae82aa3163729eec563d634
P-Burk <68164093+P-Burk@users.noreply.github.com>
2022-10-16 10:36:17 -0400

    added logic for checking for blank fields

--- app/src/main/java/com/pburkhardt/inventory_application/login.java
+++ app/src/main/java/com/pburkhardt/inventory_application/login.java
@@ -2,11 +2,16 @@ package com.pburkhardt.inventory_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class login extends AppCompatActivity {
@@ -30,7 +35,18 @@ public class login extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(login.this, "Login button pressed.", Toast.LENGTH_SHORT).show();
+                //check to see if user entered a name and password
+                if (userNameText.getText().toString().matches("") ||
+                userPasswordText.getText().toString().matches("")) {
+                    Toast.makeText(login.this, "Username and/or password fields blank.",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    InventoryUser newUser = new InventoryUser(-1, userNameText.getText().toString(),
+                            userPasswordText.getText().toString());
+                    //TODO: add function to check DB
+                    //TODO: add function to go to inventory activity if user and password in DB
+                    Toast.makeText(login.this, newUser.toString(), Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -38,7 +54,18 @@ public class login extends AppCompatActivity {
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(login.this, "Register button pressed.", Toast.LENGTH_SHORT).show();
+                //check to see if user entered a name and password
+                if (userNameText.getText().toString().matches("") ||
+                        userPasswordText.getText().toString().matches("")) {
+                    Toast.makeText(login.this, "Username and/or password fields blank.",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    InventoryUser newUser = new InventoryUser(-1, userNameText.getText().toString(),
+                            userPasswordText.getText().toString());
+                    //TODO: add function to check DB if user already exists
+                    //TODO: add function to add username and password to DB
+                    Toast.makeText(login.this, newUser.toString(), Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
