1e6ba75c6e7550e67d15d9f443de556afa7b715d
P-Burk <68164093+P-Burk@users.noreply.github.com>
2022-10-16 16:29:07 -0400

    added functionality to add item to ITEM_TABLE

--- app/src/main/java/com/pburkhardt/inventory_application/AddItemActivity.java
+++ app/src/main/java/com/pburkhardt/inventory_application/AddItemActivity.java
@@ -8,8 +8,13 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddItemActivity extends AppCompatActivity {
+    Button addButton;
+    EditText itemNameText, itemCountText;
+    DBHelper DBHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +26,26 @@ public class AddItemActivity extends AppCompatActivity {
         toolbar.setTitleTextColor(Color.WHITE);
         setSupportActionBar(toolbar);
 
-        Button addButton = findViewById(R.id.addItemButton);
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //addItemToList();
-                goToInventoryActivity(view);
-            }
-        });
-    }
+        itemNameText = findViewById(R.id.addItemName);
+        itemCountText = findViewById(R.id.addItemCount);
+        addButton = findViewById(R.id.addItemButton);
+        DBHelper = new DBHelper(AddItemActivity.this);
 
-    private void addItemToList(String newItemName, int newItemCount) {
-        //TODO: add functionality to add item to database
+        addButton.setOnClickListener(view -> {
+            inventoryItemModel newItem = new inventoryItemModel(itemNameText.getText().toString(),
+                    Integer.parseInt(itemCountText.getText().toString()));
+            if (DBHelper.checkDBforItem(newItem)) {     //check for duplicate item
+                Toast.makeText(AddItemActivity.this, "Item already in inventory.",
+                        Toast.LENGTH_SHORT).show();
+            } else {    //add item to DB if no duplicate
+                boolean addSuccess = DBHelper.addItem(newItem);
+                if (!addSuccess) {  //failed to add item
+                    Toast.makeText(AddItemActivity.this, "Failed to add new item.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+            goToInventoryActivity(view);
+        });
     }
 
     public void goToInventoryActivity(View view) {
