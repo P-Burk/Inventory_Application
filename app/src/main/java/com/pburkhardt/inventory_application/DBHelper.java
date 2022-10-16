4094cc1dfd6235c4ca0004b632d3970200f15819
P-Burk <68164093+P-Burk@users.noreply.github.com>
2022-10-16 11:40:23 -0400

    created DBHelper.java

---
+++ app/src/main/java/com/pburkhardt/inventory_application/DBHelper.java
@@ -0,0 +1,25 @@
+package com.pburkhardt.inventory_application;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    //CONSTRUCTOR
+    public DBHelper(@Nullable Context context) {
+        super(context, "InventoryApp.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
